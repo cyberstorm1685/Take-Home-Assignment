9df8ab824768890237841308b81a7975384bd66c
cyberstorm1685 <v.v.t.solutions0@gmail.com>
2025-11-05 02:58:51 +0530

    add themes, GetStorage init and app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import 'user_interface/screens/post_list_screen.dart';
 import 'utils/themes.dart';
 
 Future<void> main() async {
-  
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -13,6 +14,16 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    
+    final box = GetStorage();
+    final isDark = box.read('isDark') ?? false;
+
+    return GetMaterialApp(
+      title: 'Take Home Assignment',
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+      home: const PostListScreen(),
+    );
   }
 }
