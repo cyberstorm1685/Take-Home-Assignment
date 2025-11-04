b226bf7cae840c7aba3177a354e0d7c00c604a2a
cyberstorm1685 <v.v.t.solutions0@gmail.com>
2025-11-05 02:46:27 +0530

    Created base Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'user_interface/screens/post_list_screen.dart';
+import 'utils/themes.dart';
+
+Future<void> main() async {
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
